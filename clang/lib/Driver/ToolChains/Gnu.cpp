cc0756681aff51ec3d2a529de3f2d677e6d277c1
Vladimir Vukicevic <vladimir@pobox.com>
2021-09-19 22:44:19 -0700

    Revert "If using compiler-rt on IRIX, don't try and pull in libgcc object files"
    This reverts commit f4a9abc31aecefa3fe623a7d92297c6acab7e181.

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -411,10 +411,9 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   const bool IsPIE = getPIE(Args, ToolChain);
   const bool IsStaticPIE = getStaticPIE(Args, ToolChain);
   const bool IsStatic = getStatic(Args);
-  const bool HasCRTBeginEndFiles = IsIRIX ? 
-    ToolChain.GetRuntimeLibType(Args) != ToolChain::RLT_CompilerRT :
-    (ToolChain.getTriple().hasEnvironment() ||
-      (ToolChain.getTriple().getVendor() != llvm::Triple::MipsTechnologies));
+  const bool HasCRTBeginEndFiles =
+      ToolChain.getTriple().hasEnvironment() ||
+      (ToolChain.getTriple().getVendor() != llvm::Triple::MipsTechnologies);
 
   ArgStringList CmdArgs;
 
@@ -524,10 +523,9 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
       if (crt1)
         CmdArgs.push_back(Args.MakeArgString(ToolChain.GetFilePath(crt1)));
 
-      if (IsIRIX && 
-          ToolChain.GetRuntimeLibType(Args) != ToolChain::RLT_CompilerRT)
+      if (IsIRIX)
         CmdArgs.push_back(Args.MakeArgString(ToolChain.GetFilePath("irix-crti.o")));
-      else if (!IsIRIX)
+      else
         CmdArgs.push_back(Args.MakeArgString(ToolChain.GetFilePath("crti.o")));
     }
 
@@ -681,8 +679,7 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
         CmdArgs.push_back(Args.MakeArgString(P));
       }
       // TODO IRIX temp hack -- we need both irix-crtn.o and crtn.o
-      if (IsIRIX && 
-          ToolChain.GetRuntimeLibType(Args) != ToolChain::RLT_CompilerRT)
+      if (IsIRIX)
         CmdArgs.push_back(Args.MakeArgString(ToolChain.GetFilePath("irix-crtn.o")));
       if (!isAndroid)
         CmdArgs.push_back(Args.MakeArgString(ToolChain.GetFilePath("crtn.o")));
