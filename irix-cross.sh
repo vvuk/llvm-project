2f245d4071ded68509e0af696608c0c3c771c984
Vladimir Vukicevic <vladimir@pobox.com>
2021-09-28 16:27:12 -0700

    simplify cross

--- irix-cross.sh
+++ irix-cross.sh
@@ -1,70 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -f clang/CMakeLists.txt ] ; then
-    echo "Don't run this from the top level tree.  Make a new build directory and run with the path to the root:"
+    echo "Don't run this from the top level tree."
+    echo "Make a new build directory and run this script with a relative path:"
     echo ""
-    echo "   mkdir build-native"
-    echo "   cd build-native"
+    echo "   mkdir build-irix"
+    echo "   cd build-irix"
     echo "   ../$(basename $0)"
     exit 1
 fi
 
+ROOT=/opt/irix/root
+#ROOT=/home/vladimir/tmp/irix655
+LLVMVER=14
+
 NDIR=../build-full
-
-if [ x"$1" != "x" ] ; then
-    NDIR=$1
-    shift
-fi
-
-if [ ! -f ${NDIR}/bin/llvm-tblgen ] ; then
-    echo "Couldn't find ${NDIR}/bin/llvm-tblgen"
-    echo "Please specify path to built llvm native cross-compiler binary directory, e.g."
-    echo "   $0 ../build-full"
-    exit 1
-fi
-
-if [ ! -f /opt/irix/root/lib32/libc.so.1 ] ; then
-    echo "Expected to find IRIX root in /opt/irix/root"
-    exit 1
-fi
-
 NDIR=$(readlink -f ${NDIR})
-
-echo ${NDIR}
-
 CROSS_CC=${NDIR}/bin/clang
 CROSS_CXX=${NDIR}/bin/clang++
 XLINK="-lpthread"
 
-if [ ! -f ${CROSS_CC} ] ; then
-    echo "Can't find cross compiler ${CROSS_CC}, is it installed?"
+if [ ! -f ${ROOT}/lib32/libc.so.1 ] ; then
+    echo "Expected to find IRIX root in /opt/irix/root"
     exit 1
 fi
 
-if [ ! -f /opt/irix/root/usr/xg/include/setenv.h ] ; then
+if [ ! -f ${ROOT}/usr/xg/include/setenv.h ] ; then
     echo "libxg stuff not found, run make install in libxg"
     exit 1
 fi 
 
+#if [[ ! -f "${ROOT}/usr/lib/clang/mips64-sgi-irix6.5/gcc-irix-crti.o" || ! -f "lib/clang/${LLVMVER}.0.0/lib/mips64-sgi-irix6.5/gcc-irix-crtn.o" ]] ; then
+#    echo "${ROOT}/usr/lib/clang/mips64-sgi-irix6.5 needs to be populated with:"
+#    echo "    gcc/mips-sgi-irix6.5/9/irix-crtn.o -> gcc-irix-crtn.o"
+#    echo "    gcc/mips-sgi-irix6.5/9/irix-crti.o -> gcc-irix-crti.o"
+#    echo "    gcc/mips-sgi-irix6.5/9/include -> include"
+#    echo "    gcc/mips-sgi-irix6.5/9/include-fixed -> include-fixed"
+#    echo ""
+#    echo "    gcc-irix-crtn.o and gcc-irix-crti.o also need to be copied to lib/clang/${LLVMVER}.0.0/lib/mips64-sgi-irix6.5 (under this build dir)"
+#    exit 1
+#fi
+
 RELDIR=$(dirname $0)
 
-set -x
-
+# set -x in subshell to trace the cmake invocation
+(set -x ; \
 cmake -G Ninja \
     -DCMAKE_CROSSCOMPILING=True \
-    -C ${RELDIR}/clang/cmake/caches/IRIX-cross.cmake \
+    -C ${RELDIR}/clang/cmake/caches/IRIX.cmake \
     -DCMAKE_INSTALL_PREFIX=/usr/sgug/llvm \
-    -DGCC_INSTALL_PREFIX=/usr/sgug \
+    -DCMAKE_C_COMPILER="${CROSS_CC}" \
+    -DCMAKE_CXX_COMPILER="${CROSS_CXX}" \
     -DLLVM_TABLEGEN=${NDIR}/bin/llvm-tblgen \
     -DCLANG_TABLEGEN=${NDIR}/bin/clang-tblgen \
-    -DLLVM_OPTIMIZED_TABLEGEN=On \
-    -DCMAKE_C_COMPILER="${CROSS_CC}" -DCMAKE_CXX_COMPILER="${CROSS_CXX}" \
-    -DLLVM_INCLUDE_TESTS=OFF \
-    -DCMAKE_C_FLAGS="-I=/usr/xg/include" \
-    -DCMAKE_CXX_FLAGS="-I=/usr/xg/include" \
-    -DCMAKE_EXE_LINKER_FLAGS="-L=/usr/xg/lib32 -lxg -lgen ${XLINK}" \
-    -DCMAKE_SHARED_LINKER_FLAGS="-L=/usr/xg/lib32 -lxg -lgen ${XLINK}" \
-    -DCMAKE_MODULE_LINKER_FLAGS="-L=/usr/xg/lib32 -lxg -lgen ${XLINK}" \
+    -DDEFAULT_SYSROOT=${ROOT} \
+    -DCLANG_DEFAULT_LINKER=lld \
     $* \
     ${RELDIR}/llvm
-
+)
