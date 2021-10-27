89d0988d536e7843a23ea887441a1604beb6e082
Vladimir Vukicevic <vladimir@pobox.com>
2021-10-26 21:16:44 -0700

    fix up scripts

--- irix/build-dest.sh
+++ irix/build-dest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo running install..
 rm -rf DEST
 DESTDIR=`pwd`/DEST ninja install
 
+echo fixing includes..
 pushd DEST/usr/llvm
 cp -r ../../../../irix/include-fixed lib32/clang/14.0.0/include-fixed
 pushd lib64/clang/14.0.0
@@ -10,6 +12,14 @@ ln -s ../../../lib32/clang/14.0.0/include-fixed .
 popd
 rm -rf lib32/libLLVM*.a lib32/libclang*.a lib32/liblld*.a lib32/libfindAll*.a lib32/cmake
 rm -rf include/llvm* include/clang* include/lld* 
+popd
+
+echo moving around runtime..
+pushd DEST/usr/llvm
+cp -r lib/clang/14.0.0/lib/* lib32/clang/14.0.0/lib
+cp lib64/clang/14.0.0/lib/mips64-sgi-irix6.5/* lib32/clang/14.0.0/lib/mips64-sgi-irix6.5
+rm -rf lib64/clang/14.0.0/lib
+rm -rf lib
 popd
 
 echo Creating tarball..
--- irix/irix-crt.sh
+++ irix/irix-crt.sh
@@ -18,17 +18,24 @@ if [ "$CC" == "" ] ; then
 fi
 
 CFLAGS="-Werror=date-time -Werror=unguarded-availability-new -Wall -Wextra -Wno-unused-parameter -Wwrite-strings -Wmissing-field-initializers -Wimplicit-fallthrough -Wcovered-switch-default -Wno-comment -Wstring-conversion -Wmisleading-indentation -Wno-pedantic -fdiagnostics-color -ffunction-sections -fdata-sections -Wall -fno-lto -std=c11 -fPIC -DEH_USE_FRAME_REGISTRY"
-RTDIR=lib32/clang/14.0.0/lib/irix6.5
+
+#RTDIR=lib32/clang/14.0.0/lib/irix6.5
+RTDIR=lib32/clang/14.0.0/lib
 
 mkdir -p $RTDIR
 
 set -e
 set -x
 
-# These _must_ be named with a -xxx. 
-#${CC} ${CFLAGS} -mips3 -mabi=o32 -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin.o
-#${CC} ${CFLAGS} -mips3 -mabi=o32 -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend.o
-${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin-32.o
-${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend-32.o
-${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin-64.o
-${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend-64.o
+## These _must_ be named with a -xxx. 
+##${CC} ${CFLAGS} -mips3 -mabi=o32 -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin.o
+##${CC} ${CFLAGS} -mips3 -mabi=o32 -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend.o
+#${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin-32.o
+#${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend-32.o
+#${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/clang_rt.crtbegin-64.o
+#${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/clang_rt.crtend-64.o
+
+${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/mips64-sgi-irix6.5-gnuabin32/clang_rt.crtbegin.o
+${CC} ${CFLAGS} -mips3 -mabi=n32 -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/mips64-sgi-irix6.5-gnuabin32/clang_rt.crtend.o
+${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtbegin.c -o ${RTDIR}/mips64-sgi-irix6.5/clang_rt.crtbegin.o
+${CC} ${CFLAGS} -mips4 -mabi=64  -c ${RELDIR}/compiler-rt/lib/crt/crtend.c   -o ${RTDIR}/mips64-sgi-irix6.5/clang_rt.crtend.o
